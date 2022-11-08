2042f4b116d3387cc104f430bbcf73c9fa7cfb06
hiredtocode <jasonhan@kakao.com>
2022-11-08 23:45:24 +0900

    how to pass props from Child to Child via Parent component

---
+++ Child to Child using useContext.jsx
@@ -0,0 +1,44 @@
+//Child to Child using useContext
+
+const CountContext = React.createContext();
+
+const Counter2 = () => {
+	const { count, onIncrement, onDecrement } = React.useContext(CountContext);
+
+	return (
+		<div className="container">
+			<div className="display">{count}</div>
+			<div>
+				<button onClick={onIncrement}></button>
+				<button onClick={onDecrement}></button>
+				<ADeeperComponent />
+			</div>
+		</div>
+	);
+};
+
+const ADeeperComponent = () => {
+	const { count } = React.useContext(CountContext);
+
+	return (
+		<>
+			<div> A Deeper Component: {count}</div>
+		</>
+	);
+};
+
+const App = () => {
+	const [count, setCount] = React.useState(0);
+
+	const onIncrement = () => {
+		setCount(count + 1);
+	};
+	const onDecrement = () => {
+		setCount(count - 1);
+	};
+	return (
+		<CountContext.Provider value={{ count, onIncrement, onDecrement }}>
+			<Counter />
+		</CountContext.Provider>
+	);
+};
